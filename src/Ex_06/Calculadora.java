ac49ea24eadc27cb0df244dff61004ab5b4da4af
Jennifer-Sence <135638615+Jennifer-Sence@users.noreply.github.com>
2024-04-11 09:28:35 +0100

    commit calculadora correçao

--- src/Ex_06/Calculadora.java
+++ src/Ex_06/Calculadora.java
@@ -2,31 +2,26 @@ package Ex_06;
 
 public class Calculadora {
 
-    private int num1;
-    private int num2;
 
-    public Calculadora( int num1, int num2){
-        this.num1 = num1;
-        this.num2 = num2;
+
+    public Calculadora(){
     }
 
-    public int adicao(){
-        int resultado = num1 + num2;
-        return resultado;
+    public double adicao(double num1, double num2){
+        return num1 + num2;
+
     }
 
-    public int subtracao(){
-        int resultado = num1 - num2;
-        return resultado;
+    public double subtracao(double num1, double num2){
+        return num1 - num2;
     }
 
-    public int multiplicacao(){
-        int resultado = num1 * num2;
-        return resultado;
+    public double multiplicacao(double num1, double num2){
+        return num1 * num2;
     }
 
-    public int divisao(){
-        int resultado = num1 / num2;
-        return resultado;
+    public double divisao(double num1, double num2){
+        return num1 / num2;
+
     }
 }
--- src/Ex_06/Main.java
+++ src/Ex_06/Main.java
@@ -2,12 +2,14 @@ package Ex_06;
 
 public class Main {
     public static void main(String[] args) {
-        Calculadora calculadoraXpto = new Calculadora(10, 5);
+        double num1 = 10;
+        double num2 = 5;
+        Calculadora calculadoraXpto = new Calculadora();
         System.out.println();
-        System.out.println("Soma: " + calculadoraXpto.adicao());
-        System.out.println("Subtração: " + calculadoraXpto.subtracao());
-        System.out.println("Multiplicação: " + calculadoraXpto.multiplicacao());
-        System.out.println("Divisão: " + calculadoraXpto.divisao());
+        System.out.println("Soma: " + calculadoraXpto.adicao(num1, num2));
+        System.out.println("Subtração: " + calculadoraXpto.subtracao(num1, num2));
+        System.out.println("Multiplicação: " + calculadoraXpto.multiplicacao(num1, num2));
+        System.out.println("Divisão: " + calculadoraXpto.divisao(num1, num2));
 
     }
 }
